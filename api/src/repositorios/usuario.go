9e27c3f6903ed496fa26819f7e25322dde288f19
Luiz Fernando Bortolini <luiz.bortolini@softplan.com.br>
2022-12-20 08:11:19 -0300

    77. Criação de Usuários

--- api/src/repositorios/usuario.go
+++ api/src/repositorios/usuario.go
@@ -13,6 +13,22 @@ func NovoRepositorioUsuarios(db *sql.DB) *Usuarios {
 	return &Usuarios{db}
 }
 
-func (u Usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
-	return 0, nil
+func (repositorio Usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
+	statement, erro := repositorio.db.Prepare("insert into usuarios (nome, nick, email, senha) values (?, ?, ?, ?)")
+	if erro != nil {
+		return 0, nil
+	}
+
+	defer statement.Close()
+	resultado, erro := statement.Exec(usuario.Nome, usuario.Nick, usuario.Email, usuario.Senha)
+	if erro != nil {
+		return 0, nil
+	}
+
+	ultimoIDInserido, erro := resultado.LastInsertId()
+	if erro != nil {
+		return 0, nil
+	}
+
+	return uint64(ultimoIDInserido), nil
 }

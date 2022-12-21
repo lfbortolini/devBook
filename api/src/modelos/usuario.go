e4c7be852fb2679cf2ea011202141254d9ff9f01
Luiz Fernando Bortolini <luiz.bortolini@softplan.com.br>
2022-12-20 22:17:25 -0300

    80. Métodos para Validação de Usuário

--- api/src/modelos/usuario.go
+++ api/src/modelos/usuario.go
@@ -1,6 +1,10 @@
 package modelos
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Usuario struct {
 	ID       uint64    `json:"id,omitempty"`
@@ -9,4 +13,38 @@ type Usuario struct {
 	Email    string    `json:"email,omitempty"`
 	Senha    string    `json:"senha,omitempty"`
 	CriadoEm time.Time `json:"criadoEm,omitempty"`
+}
+
+func (usuario *Usuario) Preparar() error {
+	usuario.formatar()
+	if erro := usuario.validar(); erro != nil {
+		return erro
+	}
+
+	return nil
+}
+
+func (usuario *Usuario) validar() error {
+	if usuario.Nome == "" {
+		return errors.New("O campo nome é obrigatório")
+	}
+
+	if usuario.Nick == "" {
+		return errors.New("O campo nick é obrigatório")
+	}
+
+	if usuario.Email == "" {
+		return errors.New("O campo email é obrigatória")
+	}
+
+	if usuario.Senha == "" {
+		return errors.New("O campo senha é obrigatória")
+	}
+	return nil
+}
+
+func (usuario *Usuario) formatar() {
+	usuario.Nome = strings.TrimSpace(usuario.Nome)
+	usuario.Nick = strings.TrimSpace(usuario.Nick)
+	usuario.Email = strings.TrimSpace(usuario.Email)
 }

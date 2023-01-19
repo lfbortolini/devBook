1b25f510ea805e0ded8b026b5ef32f4022b1100a
lfbortolini <lfbortolini@gmail.com>
2023-01-18 21:51:45 -0300

    103. Métodos para Validação de Publicação

--- api/src/models/publicacao.go
+++ api/src/models/publicacao.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Publicacao struct {
 	ID        uint64    `json:"id,omitempty"`
@@ -10,4 +14,31 @@ type Publicacao struct {
 	AutorNick uint64    `json:"autorNick,omitempty"`
 	Curtidas  uint64    `json:"curtidas"`
 	CriadaEm  time.Time `json:"criadaEm,omitempty"`
+}
+
+func (publicacao *Publicacao) Preparar() error {
+	publicacao.formatar()
+
+	if erro := publicacao.validar(); erro != nil {
+		return erro
+	}
+
+	return nil
+}
+
+func (publicacao *Publicacao) validar() error {
+	if publicacao.Titulo == "" {
+		return errors.New("O campo titulo é obrigatório")
+	}
+
+	if publicacao.Conteudo == "" {
+		return errors.New("O campo conteudo é obrigatório")
+	}
+
+	return nil
+}
+
+func (publicacao *Publicacao) formatar() {
+	publicacao.Titulo = strings.TrimSpace(publicacao.Titulo)
+	publicacao.Conteudo = strings.TrimSpace(publicacao.Conteudo)
 }

80440fbf881bbebb37bf00050970e7c55966cfd9
Luiz Fernando Bortolini <luiz.bortolini@softplan.com.br>
2023-02-06 16:45:43 -0300

    125. Pacote Config

--- webapp/main.go
+++ webapp/main.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"webapp/src/config"
 	"webapp/src/router"
 	"webapp/src/utils"
 )
 
 func main() {
+	config.Carregar()
 	utils.CarregarTemplates()
 	r := router.Gerar()
 
 	fmt.Println("Rodando WebAPP!")
-	log.Fatal(http.ListenAndServe(":3000", r))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }

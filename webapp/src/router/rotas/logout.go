56e51a1735840e01453ef958eb252727af2e403e
Luiz Fernando Bortolini <luiz.bortolini@softplan.com.br>
2023-03-21 10:05:34 -0300

    145. Logout

---
+++ webapp/src/router/rotas/logout.go
@@ -0,0 +1,15 @@
+package rotas
+
+import (
+	"net/http"
+	"webapp/src/controllers"
+)
+
+var rotasLogout = []Rota{
+	{
+		URI:                "/logout",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.FazerLogout,
+		RequerAutentucacao: true,
+	},
+}
